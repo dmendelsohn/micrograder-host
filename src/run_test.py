5939b151379ba4b6be84ffc38ca521447ce69300
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2017-06-19 09:48:33 -0400

    added blinky and button test cases

--- src/run_test.py
+++ src/run_test.py
@@ -1,11 +1,15 @@
 from .case import TestCase
+from .case import TestPoint
 from .communication import SerialCommunication
 from .frame import Condition
 from .frame import ConditionType
 from .frame import Frame
 from .request import EventType
 from .request import InputType 
+from .request import OutputType
 from .response import ErrorResponse
+from .sequence import Sequence
+from .screen import Screen
 
 def run_test(test_case):
     sc = SerialCommunication()
@@ -24,35 +28,104 @@ def run_test(test_case):
 
         sc.send_response(response)
         total_log.append((request, response))
+        print(request)
+        print(response)
         if response.is_error or response.test_complete:
             break
 
-    print("Finished test, total log:")
-    for i in range(len(total_log)):
-        print("Interaction {}...".format(i))
-        req, resp = total_log[i]
-        print(req)
-        print(resp)
     return test_case.assess()
 
-def main():
+def constant_test_case():
     init_condition = Condition(ConditionType.After, 
                                cause=lambda req: req.data_type == EventType.Init)
     end_condition = Condition(ConditionType.After,
-                              cause=3000, subconditions=[init_condition])
+                              cause=1000, subconditions=[init_condition])
     frame = Frame(init_condition, end_condition,
                     inputs={
                         (InputType.DigitalRead, 13): Sequence([0],[1]),
-                        (InputType.AnalogRead, 0): Sequence([0],[1.65]),
-                        (InputType.Accelerometer, 'x'): Sequence([0], [1.5]),
+                        (InputType.AnalogRead, 0): Sequence([0],[1650]), # 1.65V
+                        (InputType.Accelerometer, 'x'): Sequence([0], [1.5]), # 1.5Gs
                         (InputType.Accelerometer, 'y'): Sequence([0], [1.6]),
                         (InputType.Accelerometer, 'z'): Sequence([0], [1.7]),
-                        (InputType.Gyroscope, 'x'): Sequence([0], [100]),
+                        (InputType.Gyroscope, 'x'): Sequence([0], [100]), # DPS
                         (InputType.Gyroscope, 'y'): Sequence([0], [110]),
                         (InputType.Gyroscope, 'z'): Sequence([0], [120]),
-                        (InputType.Magnetometer, 'x'): Sequence([0], [10000]),
+                        (InputType.Magnetometer, 'x'): Sequence([0], [10000]), # milliGauss
                         (InputType.Magnetometer, 'y'): Sequence([0], [11000]),
                         (InputType.Magnetometer, 'z'): Sequence([0], [12000])
                     })
-    case = TestCase(end_condition, frames=[frame], test_points=[], aggregators={})
+    return TestCase(end_condition, frames=[frame], test_points=[], aggregators={})
+
+def blinky_test_case(with_oled=False):
+    init_condition = Condition(ConditionType.After, 
+                               cause=lambda req: req.data_type == EventType.Init)
+    end_condition = Condition(ConditionType.After,
+                              cause=5000, subconditions=[init_condition])
+    frame = Frame(init_condition, end_condition, inputs={})
+
+    points = []
+    for i in range(1, 5):
+        if with_oled:
+            output_type, channel = OutputType.Screen, None
+            expected_value = Screen(width=128, height=64) # Blank
+            if i%2==1:
+                expected_value.paint(...)
+        else:
+            output_type, channel = OutputType.DigitalWrite, 13
+            expected_value = 0
+            if i%2==1:
+                expected_value = 1
+
+        points.append(TestPoint(
+            frame_id=0,
+            output_type=output_type,
+            channel=channel,
+            expected_value=expected_value,
+            check_interval=(i*1000 + 100,i*1000 + 900),
+            check_function=operator.eq,
+            aggregator=all))
+
+    aggs = {(OutputType.DigitalWrite, 13): all}
+    return TestCase(end_condition, frames=[frame], test_points=points, aggregators=aggs)
+
+def button_test_case(with_oled=False):
+    init_condition = Condition(ConditionType.After, 
+                               cause=lambda req: req.data_type == EventType.Init)
+    end_condition = Condition(ConditionType.After,
+                              cause=5000, subconditions=[init_condition])
+    frame = Frame(init_condition, end_condition,
+                    inputs={
+                        (InputType.DigitalRead, 6): Sequence(
+                                                        [0,1000,2000,3000,4000,5000],
+                                                        [0,1,0,1,0,1])
+                    })
+
+    points = []
+    for i in range(1, 5):
+        if with_oled:
+            output_type, channel = OutputType.Screen, None
+            expected_value = Screen(width=128, height=64) # Blank
+            if i%2==1:
+                expected_value.paint(...)
+        else:
+            output_type, channel = OutputType.DigitalWrite, 13
+            expected_value = 0
+            if i%2==1:
+                expected_value = 1
+
+        points.append(TestPoint(
+            frame_id=0,
+            output_type=output_type,
+            channel=channel,
+            expected_value=expected_value,
+            check_interval=(i*1000 + 100,i*1000 + 900),
+            check_function=operator.eq,
+            aggregator=all))
+
+    aggs = {(OutputType.DigitalWrite, 13): all}
+    return TestCase(end_condition, frames=[frame], test_points=points, aggregators=aggs)
+
+
+def main():
+    case = constant_test_case()
     result = run_test(case)
