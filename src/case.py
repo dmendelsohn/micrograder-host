e7597ba076f3958bdc61cf595eecfcb03fb94502
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2017-06-27 22:37:06 -0400

    fleshing out scaffold class a bit

--- src/case.py
+++ src/case.py
@@ -1,11 +1,22 @@
 from collections import namedtuple
+from enum import Enum
 
 TestCase = namedtuple("TestCase", ["handler", "evaluator"])
 
-#TODO: description
+class InterpolationType(Enum):
+    START = 0
+    MID = 1
+    END = 2
+    LINEAR = 3
+
+# This class stores information for constructing test cases dynamically
 class Scaffold:
-    #TODO: implement
-    pass
+    def __init__(self, interpolations, template_points, aggregators):
+        self.interpolations = interpolations # Map from (InputType,channel)->InterpolationType
+        self.template_points = template_points # Map from (data_type,channel)->TestPoint
+        self.aggregators = aggregators # Map from (data_type,channel)-> function list(bool)->bool
+        #TODO: add specification for frame triggers
+        #TODO: add specification for start/end buffers for frames
 
 # Input: log is a RequestLog
 # Input: scaffold is a Scaffold
